190f544d1c18a754d3432744cd19a794213c6e82
rowneee <roni.shabo@gmail.com>
2019-06-18 14:21:21 -0400

    working on notifications

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :owner_id, :borrower_id, :brand, :brand, :color, :size, :description, :category, :img_url, :status
-  # belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User"
 end
